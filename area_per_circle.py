7684b5a247072cbf1c4c44230a7e8cd5b0165c91
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-10-02 18:13:50 +0000

    circumference and area of a circle

---
+++ area_per_circle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Yoma Ozoh
+# Date: September 30, 2025
+# This program calculates pizza cost with user input
+
+import math
+
+
+def main():
+    rad = 77
+    circ = math.pi * 2 * rad
+    area = math.pi * rad**2
+    print(circ, area)
